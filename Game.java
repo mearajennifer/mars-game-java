59643231e9b99b33301a3569e15c9946e12ce024
Jennifer Meara <jennifer@jmeara.com>
2021-06-03 18:10:18 +0000

    add bootingUp

--- Game.java
+++ Game.java
@@ -1,5 +1,35 @@
+/*
+A text-based Mars adventure game
+*/
+
+import java.util.concurrent.TimeUnit;
+
 public class Game {
     public static void main(String[] args) {
-        System.out.println("Let's make a text-based Mars Adventure game!");
+        System.out.println("The game is about to start...");
+        bootingUp();
+
+
+    }
+
+    // method to simulate booting up the game
+    public static void bootingUp() {
+        long timeToSleep = 2L;
+        TimeUnit time = TimeUnit.SECONDS;
+        try {
+            time.sleep(timeToSleep);
+            System.out.println("Booting up...");
+            time.sleep(timeToSleep);
+            System.out.println("...");
+            time.sleep(timeToSleep);
+            System.out.println("...");
+            time.sleep(timeToSleep);
+            System.out.println("...");
+            time.sleep(timeToSleep);
+            System.out.println("Ready!\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
